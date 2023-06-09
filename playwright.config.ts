46f44190d250ce289bfc2c3206cb8230accc41ee
PenpPenp <casa2leo@gmail.com>
2023-06-09 15:09:31 +0800

    Adding more Playwright Test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+// Read from default ".env" file.
+dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -25,7 +23,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    //baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.URL === 'playwright' ? 'https://playwright.dev' : 'https://www.google.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
